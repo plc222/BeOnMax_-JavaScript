78950d4c47a2a5b249b8247a072baaad69314feb
plc222 <yurij.altay220@yandex.ru>
2021-07-03 18:10:42 +0700

    Создал js-code

--- js/less_2.7.js
+++ js/less_2.7.js
@@ -1,5 +1,21 @@
-let a = 45;
-console.log(a);
-let b = 34;
-let result = a + b;
-console.log(result);
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
